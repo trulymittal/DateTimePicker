58008f70448935a53ef692aa62c20a0c09c804e2
Truly Mittal <trulymittal@gmail.com>
2019-03-19 01:48:50 +0530

    starter project

--- app/src/main/java/com/example/datetime/MainActivity.java
+++ app/src/main/java/com/example/datetime/MainActivity.java
@@ -2,12 +2,44 @@ package com.example.datetime;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button dateButton, timeButton;
+    TextView dateTextView, timeTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dateButton.findViewById(R.id.dateButton);
+        timeButton.findViewById(R.id.timeButton);
+        dateTextView.findViewById(R.id.dateTextView);
+        timeTextView.findViewById(R.id.timeTextView);
+
+        dateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleDateButton();
+            }
+        });
+        timeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleTimeButton();
+            }
+        });
     }
+
+    private void handleTimeButton() {
+    }
+
+    private void handleDateButton() {
+    }
+
+
 }
